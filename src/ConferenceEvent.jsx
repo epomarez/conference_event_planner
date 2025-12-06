6639744c4c9089d9ed2230ee0e2e5f8722902948
Edrey Pomarez <pomarezedrey@gmail.com>
2025-12-05 23:22:24 -0500

    Complete addons section

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -56,10 +56,15 @@ const ConferenceEvent = () => {
             venueItems.forEach((item) => {
                 totalCost += item.cost * item.quantity;
             });
+        } else if (section === "av"){
+            avItems.forEach((item)=>{
+                totalCost += item.cost * item.quantity;
+            })
         }
         return totalCost;
     };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -179,7 +184,7 @@ const ConferenceEvent = () => {
                                     
                                     }
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: ${avTotalCost}</div>
 
                             </div>
 
